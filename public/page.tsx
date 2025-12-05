dc18b063d9cf314f5fcfc4be7d031bbd7030ba3d
Ajosenildo <ajosenildosilva80@yahoo.com>
2025-12-05 13:51:18 -0300

    Versão 1.19

---
+++ public/page.tsx
@@ -0,0 +1,9 @@
+// Caminho: src/app/teste/page.tsx
+export default function TestePage() {
+  return (
+    <div style={{ padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Teste de Rota</h1>
+      <p>Se você está lendo isso, o servidor Vercel está funcionando.</p>
+    </div>
+  )
+}
